5c277ed1013f48bd02eef0215afccc3965e50684
noorjaan <18noorjaan@gmail.com>
2020-03-11 04:04:04 +0530

    added a function hello

--- Hellocpp.cpp
+++ Hellocpp.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 using namespace std;
 
+void hello() {
+	cout << "hello";
+}
 
 int main() {
 	cout << "hello programmers!\n";
 	cout << "Adding one more line here\n";
+	hello();
 	return 0;
 }
